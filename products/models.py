5b553f018a1da86c4b4843f3e9ba63135d1ac4f0
Samuelyip74 <samuel.yip@live.com>
2019-07-04 07:47:35 +0800

    commit

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     slug            = models.SlugField(blank=True, unique=True)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
-    #image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    image           = models.FileField(upload_to='products/', null=True, blank=True)
     featured        = models.BooleanField(default=False)
     active          = models.BooleanField(default=True)
     timestamp       = models.DateTimeField(auto_now_add=True)
--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,5 @@
+
+
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
@@ -10,3 +12,4 @@ urlpatterns = [
     url(r'^$',ProductListView.as_view(), name='list'),
     url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(), name='detail'),
 ]
+
