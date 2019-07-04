4eb85f30aa66e6595290c6f51dbe3eaad3bf3df4
Samuelyip74 <samuel.yip@live.com>
2019-07-04 23:56:08 +0800

    commit

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,3 @@
-
-
-from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
 
@@ -14,10 +11,7 @@ from products.views import (
 app_name = 'products'
 
 urlpatterns = [
-    url(r'^featured/$',ProductFeaturedListView.as_view(), name='list'),
-    url(r'^featured/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(), name='featuredetail'),
     url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(), name='detail'),
     url(r'^$',ProductListView.as_view(), name='list'),
-
 ]
 
